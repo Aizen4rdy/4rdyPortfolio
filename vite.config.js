b2abfc901b5c2ed1b0d089a83f4550384451ddbf
Ford Bryant B. Sadio <tinaysadio@gmail.com>
2025-07-22 15:49:03 +0800

    Fix: updated base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
+// ✅ Corrected Vercel-compatible config
 export default defineConfig({
-  base: "/cv-resume/",
+  base: "./",
   plugins: [react(), tailwindcss()],
   build: { outDir: "dist" },
-
   assetsInclude: ["**/*.ttf", "**/*.woff", "**/*.woff2"],
 });
